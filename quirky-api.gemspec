ddfd725276ac58c72637cc85e947a6650920020f
Michael Chittenden <mchitten@gmail.com>
2014-08-19 13:43:35 -0400

    Stubbing

--- quirky-api.gemspec
+++ quirky-api.gemspec
@@ -24,11 +24,12 @@ Gem::Specification.new do |s|
   s.add_dependency 'typhoeus'
   s.add_dependency 'yajl-ruby'
   s.add_dependency 'virtus'
+  s.add_dependency 'webmock'
+  s.add_dependency 'faker'
 
   s.add_development_dependency 'rails', '4.1.4'
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'capybara'
-  s.add_development_dependency 'faker'
   s.add_development_dependency 'factory_girl_rails'
 end
---
+++ spec/lib/stub_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe QuirkyApi do
+  context 'when stubbing' do
+    before do
+      allow(QuirkyApi).to receive(:stub_requests).and_return(true)
+      allow(QuirkyApi::Client).to receive(:qc_host).and_return('http://qc-test.local')
+      require 'quirky-api/stub'
+    end
+
+    let(:client) { QuirkyApi::Client.new(qc_host: 'http://qc-test.local', qtip_host: 'http://qtip-test.local') }
+
+    it 'should stub a user find call for user 1' do
+      user = client.users.find(1)
+      expect(user.email).to eq('admin@quirky.com')
+    end
+
+    it 'should stub a user find call for user 2' do
+      user = client.users.find(2)
+      expect(user.email).to eq('user@quirky.com')
+    end
+
+    it 'should stub a valid user create call with a successful request' do
+      user = client.users.create(email: 'cbrady@quirky.com', first_name: 'Chris', last_name: 'Brady', password: 'password')
+      expect(user).to be_a(QuirkyApi::User)
+    end
+
+    it 'should stub an invalid user create call with an error response' do
+      expect { client.users.create({}) }.to raise_error
+    end
+  end
+end
